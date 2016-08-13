3c8eb90070dc09f8702a29eeb9f55a76279fcb9b
Salomon <salomonpluviose@yahoo.com>
2016-08-12 21:48:59 -0400

    late commit but started social media web app with authentication and profiles controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def find_user
+    @user = User.find_by(params[:id])
+  end
+end
